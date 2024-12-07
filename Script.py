8bc81b4955cd9c9e9d635de0cba7a999a14f4482
──「 ʟᴜᴄɪғᴇʀ 」── <nimovie09@gmail.com>
2024-12-07 22:50:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -107,7 +107,7 @@ User - {}"""
 ❗️ ʟᴀɴɢᴜᴀɢᴇ - {languages}</b>
 """
 
-    FILE_CAPTION = """<a href='https://t.me/bisal_files'><b>ɴᴀᴍᴇ : {file_name}</b></a>"""
+    FILE_CAPTION = """<b><a href='https://t.me/Ni_Movies'><b>ɴᴀᴍᴇ : {file_name}</b></a>\n\nPowered By : <a href="https://t.me/Ni_Movies">NI MOVIES</a><\b>"""
 
     RESTART_TXT = """<b>
 📅 Dᴀᴛᴇ : <code>{}</code>
